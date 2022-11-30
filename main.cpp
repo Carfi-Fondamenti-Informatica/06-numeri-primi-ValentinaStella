60ebd956e0b9dee69935459076e6028c7a158394
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2022-11-30 19:17:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
 
-int main(){
-  return 0;
+    primo(n) ? cout<< "numero primo\n" : cout<<"numero non primo\n";
+
+    return 0;
 }
