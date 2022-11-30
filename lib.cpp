2c66133ebfa51766d7bcf0635b86819ce3e7a9f7
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2022-11-30 19:18:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+bool primo (int n){
+    if (n>1){
+        for (int i = n-1; i >1 ; --i) {
+            if((n%i)==0)
+                return false;
+        }
+        return true;
+    }
+    return false;
+}
